de0bdaeb63ae6cd459128891874d6281c258bce3
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2023-03-14 21:38:10 -0300

    .

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const fs = require("fs");
+var superheroes = require("superheroes");
 
+var mal = require("supervillains");
+
+var mysupername = superheroes.random();
+
+var myvillainname = mal.random();
+
+console.log(myvillainname);
